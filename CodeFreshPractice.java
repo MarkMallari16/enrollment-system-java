98a3a102643560717475e55583449e9204644a04
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2025-02-19 22:23:20 +0800

    First Commit

---
+++ CodeFreshPractice.java
@@ -0,0 +1,11 @@
+package codefreshpractice;
+
+public class CodeFreshPractice {
+
+    public static void main(String[] args) {
+        Enrollment en = new Enrollment();
+        
+        en.show();
+    }
+
+}
